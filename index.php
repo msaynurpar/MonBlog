ac0283dd4aa7078a495d27d4a754011de0a34767
msaynurpar <msaynurpar@gmail.com>
2014-02-25 15:25:52 +0100

    Modification MVC

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-// Accès aux données
-$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'root', '');
-$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
-        . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-        . ' order by BIL_ID desc');
-?>
 
-// Affichage
+require 'Modele.php';
+
+$billets = getBillets();
+
 require 'vueAccueil.php';
